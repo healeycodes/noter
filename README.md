27d48745a5200011d3090e1da1b45bda5b0b353e
Andrew Healey <healeycodes@gmail.com>
2023-01-24 14:25:12 +0000

    Reach feature parity (ish) with nano

--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 Fonts are from: https://github.com/TakWolf/ark-pixel-font
 
 TODO:
-- stop navigating past the bottom
-- line wrap
-- scrolling
-- time/weather
+- Scrolling improvements? Maybe add some "intelligence" here.
+  - Rather than manual scrolling, let's scroll the page when the cursor goes under, or above, the view
+  - Scroll into view when user types off-screen
--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 	"log"
 	"math"
 	"os"
+	"os/exec"
+	"path/filepath"
 	"strconv"
 	"strings"
 
@@ -19,7 +21,8 @@ import (
 )
 
 var (
-	filepath string
+	filePath string
+	fileName string
 	//go:embed fonts/dist/fonts.store
 	fontStoreRaw []byte
 	//go:embed fonts/dist/fonts.json
@@ -41,9 +44,10 @@ type Cursor struct {
 }
 
 type Editor struct {
-	start  *Line
-	cursor *Cursor
-	scroll int
+	start    *Line
+	cursor   *Cursor
+	scroll   int
+	modified bool
 }
 
 func init() {
@@ -71,48 +75,55 @@ func init() {
 }
 
 func (e *Editor) Load() error {
-	e.scroll = 0
-	e.start = &Line{values: make([]rune, 0)}
-
-	f, err := os.Open(filepath)
+	f, err := os.Open(filePath)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	defer f.Close()
 
-	b, err := os.ReadFile(filepath)
+	fileName = filepath.Base(filePath)
+	b, err := os.ReadFile(filePath)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	source := []rune(string(b))
+	source := string(b)
 
-	// Turn empty files into `\n`
-	// Also, all files must end with `\n`
-	if len(source) == 0 || source[len(source)-1] != rune('\n') {
-		source = append(source, rune('\n'))
-	}
-
-	currentLine := e.start
+	e.modified = false
+	e.scroll = 0
+	e.start = &Line{values: make([]rune, 0)}
 	e.cursor = &Cursor{line: e.start, x: 0}
+	currentLine := e.start
 
-	for _, char := range source {
-		currentLine.values = append(currentLine.values, char)
-		if char == '\n' {
-			nextLine := &Line{values: make([]rune, 0)}
-			currentLine.next = nextLine
-			nextLine.prev = currentLine
-			currentLine = nextLine
+	if len(source) == 0 {
+		currentLine.values = append(currentLine.values, '\n')
+	} else {
+		for _, char := range source {
+			currentLine.values = append(currentLine.values, char)
+			if char == '\n' {
+				nextLine := &Line{values: make([]rune, 0)}
+				currentLine.next = nextLine
+				nextLine.prev = currentLine
+				currentLine = nextLine
+			}
 		}
 	}
 
-	// TODO: make next line nil?
+	// Ensure the final line ends with `\n`
+	if len(currentLine.values) > 0 && currentLine.values[len(currentLine.values)-1] != '\n' {
+		currentLine.values = append(currentLine.values, '\n')
+	}
+
+	// Remove dangling line
+	if currentLine.prev != nil {
+		currentLine.prev.next = nil
+	}
 
 	return nil
 }
 
-func (e *Editor) Update() error {
-	if inpututil.IsKeyJustPressed(ebiten.KeyEnter) {
+func (e *Editor) HandleRune(r rune) {
+	if r == '\n' {
 		before := e.cursor.line
 		after := e.cursor.line.next
 
@@ -120,7 +131,7 @@ func (e *Editor) Update() error {
 		leftBehindValues := make([]rune, 0)
 		shiftedValues = append(shiftedValues, e.cursor.line.values[e.cursor.x:]...)
 		leftBehindValues = append(leftBehindValues, e.cursor.line.values[:e.cursor.x]...)
-		leftBehindValues = append(leftBehindValues, rune('\n'))
+		leftBehindValues = append(leftBehindValues, '\n')
 		e.cursor.line.values = leftBehindValues
 
 		e.cursor.line = &Line{
@@ -136,8 +147,117 @@ func (e *Editor) Update() error {
 		if after != nil {
 			after.prev = e.cursor.line
 		}
+	} else {
+		modifiedLine := make([]rune, 0)
+		modifiedLine = append(modifiedLine, e.cursor.line.values[:e.cursor.x]...)
+		modifiedLine = append(modifiedLine, r)
+		modifiedLine = append(modifiedLine, e.cursor.line.values[e.cursor.x:]...)
+		e.cursor.line.values = modifiedLine
+		e.cursor.x++
 	}
 
+	e.modified = true
+}
+
+func (e *Editor) Update() error {
+	// Log key number
+	// for i := 0; i < int(ebiten.KeyMax); i++ {
+	// 	if inpututil.IsKeyJustPressed(ebiten.Key(i)) {
+	// 		println(i)
+	// 		return nil
+	// 	}
+	// }
+
+	// Quit
+	if inpututil.IsKeyJustPressed(ebiten.KeyQ) && ebiten.IsKeyPressed(ebiten.KeyMetaLeft) {
+		os.Exit(0)
+		return nil
+	}
+
+	// Save
+	if inpututil.IsKeyJustPressed(ebiten.KeyS) && ebiten.IsKeyPressed(ebiten.KeyMetaLeft) {
+		allRunes := e.GetAllRunes()
+		saveFile, err := os.Create(filePath)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		_, err = saveFile.Write([]byte(string(allRunes)))
+		if err != nil {
+			log.Fatalln(err)
+		}
+		e.modified = false
+		return nil
+	}
+
+	// Paste
+	if inpututil.IsKeyJustPressed(ebiten.KeyV) && ebiten.IsKeyPressed(ebiten.KeyMetaLeft) {
+		pasteBytes, err := macOSpaste()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		for _, r := range string(pasteBytes) {
+			e.HandleRune(r)
+		}
+		return nil
+	}
+
+	// Cut line
+	if inpututil.IsKeyJustPressed(ebiten.KeyX) && ebiten.IsKeyPressed(ebiten.KeyMetaLeft) {
+		copyBytes := []byte(string(e.cursor.line.values))
+		err := macOScopy(copyBytes)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		// Remove the line
+		if e.cursor.line.prev == nil && e.cursor.line.next == nil {
+			e.cursor.line.values = []rune{'\n'}
+		} else {
+			if e.cursor.line.next == nil {
+				e.cursor.line.prev.next = nil
+				e.cursor.line = e.cursor.line.prev
+			} else if e.cursor.line.prev == nil {
+				// Make sure that e.start isn't pointing to a 'dead line'
+				e.start = e.cursor.line.next
+				e.cursor.line.next.prev = nil
+				e.cursor.line = e.cursor.line.next
+			} else {
+				e.cursor.line.prev.next = e.cursor.line.next
+				e.cursor.line.next.prev = e.cursor.line.prev
+				e.cursor.line = e.cursor.line.next
+			}
+
+			e.cursor.x = int(math.Min(float64(e.cursor.x), float64(len(e.cursor.line.values)-1)))
+		}
+		return nil
+	}
+
+	// Copy all
+	if inpututil.IsKeyJustPressed(ebiten.KeyC) && ebiten.IsKeyPressed(ebiten.KeyMetaLeft) {
+		allRunes := e.GetAllRunes()
+		copyBytes := []byte(string(allRunes))
+		err := macOScopy(copyBytes)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		return nil
+	}
+
+	// Enter
+	if inpututil.IsKeyJustPressed(ebiten.KeyEnter) {
+		e.HandleRune('\n')
+		return nil
+	}
+
+	// Tab (just insert four spaces)
+	if inpututil.IsKeyJustPressed(ebiten.KeyTab) {
+		for i := 0; i < 4; i++ {
+			e.HandleRune(' ')
+		}
+		return nil
+	}
+
+	// Backspace
 	if inpututil.IsKeyJustPressed(ebiten.KeyBackspace) {
 		if e.cursor.x == 0 {
 			if e.cursor.line.prev != nil {
@@ -154,9 +274,35 @@ func (e *Editor) Update() error {
 			e.cursor.x--
 			e.cursor.line.values = append(e.cursor.line.values[:e.cursor.x], e.cursor.line.values[e.cursor.x+1:]...)
 		}
+		e.modified = true
+		return nil
 	}
 
+	// Scroll
+	if inpututil.IsKeyJustPressed(ebiten.KeyArrowUp) && ebiten.IsKeyPressed(ebiten.KeyShift) {
+		if e.scroll > 0 {
+			e.scroll--
+			if e.cursor.line.prev != nil {
+				e.cursor.line = e.cursor.line.prev
+			}
+		}
+		return nil
+	} else if inpututil.IsKeyJustPressed(ebiten.KeyArrowDown) && ebiten.IsKeyPressed(ebiten.KeyShift) {
+		e.scroll++
+		if e.cursor.line.next != nil {
+			e.cursor.line = e.cursor.line.next
+		}
+		return nil
+	}
+
+	// Movement
 	if inpututil.IsKeyJustPressed(ebiten.KeyArrowRight) {
+		if ebiten.IsKeyPressed(ebiten.KeyMetaLeft) {
+			for e.cursor.x < len(e.cursor.line.values)-1 {
+				e.cursor.x++
+			}
+			return nil
+		}
 		if e.cursor.x < len(e.cursor.line.values)-1 {
 			e.cursor.x++
 		} else if e.cursor.line.next != nil {
@@ -164,6 +310,10 @@ func (e *Editor) Update() error {
 			e.cursor.x = 0
 		}
 	} else if inpututil.IsKeyJustPressed(ebiten.KeyArrowLeft) {
+		if ebiten.IsKeyPressed(ebiten.KeyMetaLeft) {
+			e.cursor.x = 0
+			return nil
+		}
 		if e.cursor.x > 0 {
 			e.cursor.x--
 		} else if e.cursor.line.prev != nil {
@@ -171,11 +321,23 @@ func (e *Editor) Update() error {
 			e.cursor.x = len(e.cursor.line.values) - 1
 		}
 	} else if inpututil.IsKeyJustPressed(ebiten.KeyArrowUp) {
+		if ebiten.IsKeyPressed(ebiten.KeyMetaLeft) {
+			for e.cursor.line.prev != nil {
+				e.cursor.line = e.cursor.line.prev
+			}
+			return nil
+		}
 		if e.cursor.line.prev != nil {
 			e.cursor.x = int(math.Min(float64(e.cursor.x), float64(len(e.cursor.line.prev.values)-1)))
 			e.cursor.line = e.cursor.line.prev
 		}
 	} else if inpututil.IsKeyJustPressed(ebiten.KeyArrowDown) {
+		if ebiten.IsKeyPressed(ebiten.KeyMetaLeft) {
+			for e.cursor.line.next != nil {
+				e.cursor.line = e.cursor.line.next
+			}
+			return nil
+		}
 		if e.cursor.line.next != nil {
 			e.cursor.x = int(math.Min(float64(e.cursor.x), float64(len(e.cursor.line.next.values)-1)))
 			e.cursor.line = e.cursor.line.next
@@ -199,15 +361,60 @@ func (e *Editor) Update() error {
 				continue
 			}
 
-			modifiedLine := make([]rune, 0)
-			modifiedLine = append(modifiedLine, e.cursor.line.values[:e.cursor.x]...)
-			modifiedLine = append(modifiedLine, keyRune)
-			modifiedLine = append(modifiedLine, e.cursor.line.values[e.cursor.x:]...)
-			e.cursor.line.values = modifiedLine
-			e.cursor.x++
+			e.HandleRune(keyRune)
 		}
 	}
 	return nil
+}
+
+func (e *Editor) GetAllRunes() []rune {
+	all := make([]rune, 0)
+	cur := e.start
+	for cur != nil {
+		all = append(all, cur.values...)
+		cur = cur.next
+	}
+	return all
+}
+
+func macOScopy(copyBytes []byte) error {
+	cmd := exec.Command("pbcopy")
+	in, err := cmd.StdinPipe()
+	if err != nil {
+		return err
+	}
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+	if _, err := in.Write(copyBytes); err != nil {
+		return err
+	}
+	if err := in.Close(); err != nil {
+		return err
+	}
+	if err := cmd.Wait(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func macOSpaste() ([]byte, error) {
+	cmd := exec.Command("pbpaste")
+	pasteBytes, err := cmd.Output()
+	if err != nil {
+		return nil, err
+	}
+	return pasteBytes, nil
+}
+
+func (e *Editor) GetLineNumber() int {
+	cur := e.start
+	count := 1
+	for cur != e.cursor.line {
+		count++
+		cur = cur.next
+	}
+	return count
 }
 
 func (e *Editor) Draw(screen *ebiten.Image) {
@@ -222,10 +429,17 @@ func (e *Editor) Draw(screen *ebiten.Image) {
 
 	// How many lines of text can be displayed
 	// (there's yPadding for top and bottom bar)
-	slots := (yLayout - int(yPadding*2)) / yUnit
+	xSlots := (yLayout - int(yPadding*2)) / yUnit
+
+	// How many characters can be displayed width-wise
+	// ySlots := (xLayout - int(xPadding*2)) / xUnit
 
 	// Handle top bar
-	topBar := []rune("file.txt (modified)")
+	modifiedText := ""
+	if e.modified {
+		modifiedText = "(modified)"
+	}
+	topBar := []rune(fmt.Sprintf("%s %s", fileName, modifiedText))
 	for x, char := range topBar {
 		opts := &ebiten.DrawImageOptions{}
 		opts.GeoM.Translate(float64(x*xUnit)+xPadding, 0)
@@ -242,7 +456,7 @@ func (e *Editor) Draw(screen *ebiten.Image) {
 	})
 
 	// Handle bottom bar
-	botBar := []rune("^+x (save&quit) ^+k (cut line) ^+v (paste) ^+q (quit)")
+	botBar := []rune(fmt.Sprintf("(x cut, v paste, s save, q quit, c copyall) [%v:%v] ", e.GetLineNumber(), e.cursor.x+1))
 	for x, char := range botBar {
 		opts := &ebiten.DrawImageOptions{}
 		opts.GeoM.Translate(float64(x*xUnit)+xPadding, float64(yLayout-yUnit))
@@ -271,26 +485,45 @@ func (e *Editor) Draw(screen *ebiten.Image) {
 
 	for curLine != nil {
 		// Don't render outside the line area
-		if y == slots {
+		if y == xSlots {
 			break
 		}
 
-		// Handle one line
-		for x, char := range curLine.values {
+		// Handle each line
+		x := 0
+		xOverSpilled := false
+		for _, char := range curLine.values {
 			opts := &ebiten.DrawImageOptions{}
-			opts.GeoM.Translate(float64(x*xUnit)+xPadding, float64(y*yUnit)+yPadding)
 
 			// Render cursor
 			if e.cursor.line == curLine && x == e.cursor.x {
+				// If we're about to render off the edge of the screen
+				// it means we're trying to view a very long line.
+				// Clear the current line and start from zero (hiding everything to the left)
+				if float64(x*xUnit)+xPadding >= float64(xLayout) {
+					if xOverSpilled {
+						// Only clear the current line once, otherwise stop rendering
+						// otherwise we'll get visual bugs
+						break
+					}
+					x = 0
+					xOverSpilled = true
+					ebitenutil.DrawRect(screen, 0, float64(y*yUnit)+yPadding, float64(xLayout), float64(yUnit), color.RGBA{
+						255, 255, 255, 255,
+					})
+				}
+
+				// Draw grey cursor background
 				ebitenutil.DrawRect(screen, float64(x*xUnit)+xPadding, float64(y*yUnit)+yPadding, float64(xUnit), float64(yUnit), color.RGBA{
 					0, 0, 0, 100,
 				})
 			}
 
+			opts.GeoM.Translate(float64(x*xUnit)+xPadding, float64(y*yUnit)+yPadding)
 			if char != '\n' {
 				fontImage, ok := fontImages[char]
 				if !ok {
-					// Render a red square for unknown characters (like `\t`)
+					// Render a red square [?] for unknown characters
 					ebitenutil.DrawRect(screen, float64(x*xUnit)+xPadding, float64(y*yUnit)+yPadding, float64(xUnit), float64(yUnit), color.RGBA{
 						90, 0, 0, 60,
 					})
@@ -299,6 +532,7 @@ func (e *Editor) Draw(screen *ebiten.Image) {
 					screen.DrawImage(fontImage, opts)
 				}
 			}
+			x++
 		}
 		curLine = curLine.next
 		y++
@@ -306,7 +540,8 @@ func (e *Editor) Draw(screen *ebiten.Image) {
 }
 
 func (e *Editor) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 320
+	_xScreen, _yScreen := ebiten.WindowSize()
+	return _xScreen / 2, _yScreen / 2
 }
 
 func main() {
@@ -314,9 +549,11 @@ func main() {
 		fmt.Println("usage: noter <filepath>")
 		os.Exit(1)
 	} else if len(os.Args) == 3 {
-		filepath = os.Args[2]
+		// Allow `go run . -- a.txt` for now..
+		filePath = os.Args[2]
 	} else {
-		filepath = os.Args[1]
+		// This is the way
+		filePath = os.Args[1]
 	}
 
 	editor := &Editor{}
@@ -325,13 +562,14 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	ebiten.SetWindowSize(640, 640)
+	ebiten.SetWindowSize(800, 500)
 	ebiten.SetWindowTitle("noter")
 	if err = ebiten.RunGame(editor); err != nil {
 		log.Fatalln(err)
 	}
 }
 
+// Supports macOS UK keyboard
 func keyToRune(k ebiten.Key, shift bool) (rune, bool) {
 	ret := ""
 
@@ -522,8 +760,6 @@ func keyToRune(k ebiten.Key, shift bool) (rune, bool) {
 	// Spacing
 	case ebiten.KeySpace:
 		ret = " "
-	case ebiten.KeyTab:
-		ret = "\t"
 	}
 
 	// Handle case (only affects alphas)
