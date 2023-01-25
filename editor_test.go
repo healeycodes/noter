ba5cdc645397a3f18a33d4fdcf3441bb0c4286aa
Andrew Healey <healeycodes@gmail.com>
2023-01-25 13:57:31 +0000

    Add tests for GetLineNumber and GetAllRunes

--- editor_test.go
+++ editor_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/hajimehoshi/ebiten/v2"
@@ -20,5 +21,43 @@ func TestKeyToRune(t *testing.T) {
 	r, printable = KeyToRune(ebiten.KeyEnter, false)
 	if r != 0 && !printable {
 		t.Fatalf(`Expected '0' and printable: false, got: %v %v`, r, printable)
+	}
+}
+
+func TestGetLineNumber(t *testing.T) {
+	line1 := &Line{}
+	line2 := &Line{}
+	line1.next = line2
+	line2.prev = line1
+	editor := &Editor{
+		start: line1,
+		cursor: &Cursor{
+			line2,
+			0,
+		},
+	}
+
+	lineNum := editor.GetLineNumber()
+	if lineNum != 2 {
+		t.Fatalf(`Expected current line number to be 2, got: %v`, lineNum)
+	}
+}
+
+func TestGetAllRunes(t *testing.T) {
+	line1 := &Line{values: []rune{'a', '\n'}}
+	line2 := &Line{values: []rune{'b', '\n'}}
+	line1.next = line2
+	line2.prev = line1
+	editor := &Editor{
+		start: line1,
+		cursor: &Cursor{
+			line2,
+			0,
+		},
+	}
+
+	allRunes := editor.GetAllRunes()
+	if reflect.DeepEqual(allRunes, []rune{'a', '\n', 'b', '\n'}) != true {
+		t.Fatalf(`Expected allRunes to return document runes, got: %v`, allRunes)
 	}
 }
